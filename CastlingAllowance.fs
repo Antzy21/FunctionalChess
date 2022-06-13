4682eb253fa5a19968ba110a9e98b737434b1297
Anthony Dunford <trashzy21@gmail.com>
2022-06-13 23:36:19 +0100

    Pull out "castlingAllowance" and "side"

---
+++ CastlingAllowance.fs
@@ -0,0 +1,22 @@
+﻿namespace Chess
+
+open Checkerboard
+
+type castlingAllowance = {whiteKingside: bool; whiteQueenside: bool; blackKingside: bool; blackQueenside: bool;}
+
+module CastlingAllowance =
+    let fromFen (fen: string) : castlingAllowance =
+        {
+            whiteKingside = fen.Contains('K');
+            whiteQueenside = fen.Contains('Q');
+            blackKingside = fen.Contains('k');
+            blackQueenside = fen.Contains('q')
+        }
+    let removeRights (colour: colour) (side: side) (ca: castlingAllowance) : castlingAllowance =
+        match colour, side with
+        | White, Kingside -> {whiteKingside = false; whiteQueenside = ca.whiteQueenside; blackKingside = ca.blackKingside; blackQueenside = ca.blackQueenside}
+        | White, Queenside -> {whiteKingside = ca.whiteKingside; whiteQueenside = false; blackKingside = ca.blackKingside; blackQueenside = ca.blackQueenside}
+        | Black, Kingside -> {whiteKingside = ca.whiteKingside; whiteQueenside = ca.whiteQueenside; blackKingside = false; blackQueenside = ca.blackQueenside}
+        | Black, Queenside -> {whiteKingside = ca.whiteKingside; whiteQueenside = ca.whiteQueenside; blackKingside = ca.blackKingside; blackQueenside = false}
+    let removeBasedOnMove (move: move) (ca: castlingAllowance) : castlingAllowance =
+        removeRights (Move.getMovedPiece move |> fun p -> p.colour) (Move.getCastlingSide move) ca
---
+++ side.fs
@@ -0,0 +1,5 @@
+﻿namespace Chess
+
+type side =
+    | Kingside
+    | Queenside
