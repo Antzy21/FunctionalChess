6cf55e805a7bbcb448f79457b641f875358d9d44
Anthony Dunford <trashzy21@gmail.com>
2022-06-13 19:31:16 +0100

    Pull out PieceType into own file

---
+++ PieceType.fs
@@ -0,0 +1,27 @@
+﻿namespace Chess
+
+type pieceType =
+    | Pawn
+    | Rook
+    | Knight
+    | Bishop
+    | King
+    | Queen
+
+module PieceType =
+    let getValue (piece: pieceType) : int option =
+        match piece with
+        | Pawn -> Some 1
+        | Rook -> Some 5
+        | Knight -> Some 3
+        | Bishop -> Some 3
+        | King -> None
+        | Queen -> Some 9
+    let getLetter (piece: pieceType) : char =
+        match piece with
+        | Pawn -> 'P'
+        | Rook -> 'R'
+        | Knight -> 'N'
+        | Bishop -> 'B'
+        | King -> 'K'
+        | Queen -> 'Q'
--- piece.fs
+++ piece.fs
@@ -3,32 +3,6 @@ namespace Chess
 open Checkerboard
 open FSharp.Extensions
 
-type pieceType =
-    | Pawn
-    | Rook
-    | Knight
-    | Bishop
-    | King
-    | Queen
-
-module PieceType =
-    let getValue (piece: pieceType) : int option =
-        match piece with
-        | Pawn -> Some 1
-        | Rook -> Some 5
-        | Knight -> Some 3
-        | Bishop -> Some 3
-        | King -> None
-        | Queen -> Some 9
-    let getLetter (piece: pieceType) : char =
-        match piece with
-        | Pawn -> 'P'
-        | Rook -> 'R'
-        | Knight -> 'N'
-        | Bishop -> 'B'
-        | King -> 'K'
-        | Queen -> 'Q'
-
 type piece = {pieceType: pieceType; colour: colour}
 
 type pieceMoveGenerator = (int * int) -> (int * int) list
