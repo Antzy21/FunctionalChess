fa738e8854e688fa51bee19c84502eb2c1415b82
Anthony Dunford <trashzy21@gmail.com>
2022-06-14 18:40:52 +0100

    Setup testing helpers to get specific moves

---
+++ ChessTest/TestHelpers.fs
@@ -0,0 +1,10 @@
+﻿module TestHelpers
+
+open Chess
+
+let internal getPossibleCastlingMoveNotations (fen: string) =
+    fen
+    |> GameState.fromFen
+    |> GameState.getMovesForPlayer
+    |> List.filter Move.isCastling
+    |> List.map Move.getMoveNotation
