e1a7d01f39b071c53d73d2def90ba5b5468e4d97
Anthony Dunford <trashzy21@gmail.com>
2022-06-14 19:01:40 +0100

    Fix Actual vs Expected order in AssertEqual test

--- ChessTest/Castling.fs
+++ ChessTest/Castling.fs
@@ -8,31 +8,31 @@ module White =
     [<Fact>]
     let ``White castling both sides possible`` () =
         let castlingMoves = getPossibleCastlingMoveNotations "r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R w KQkq - 1 2"
-        Assert.StrictEqual<string list>(castlingMoves, ["0-0-0"; "0-0"])
+        Assert.StrictEqual<string list>(["0-0-0"; "0-0"], castlingMoves)
 
     [<Fact>]
     let ``White castling not allowed from gameState`` () =
         let castlingMoves = getPossibleCastlingMoveNotations "r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R w kq - 1 2"
-        Assert.StrictEqual<string list>(castlingMoves, [])
+        Assert.StrictEqual<string list>([], castlingMoves)
         
     [<Fact>]
     let ``White castling blocked by piece`` () =
         let castlingMoves = getPossibleCastlingMoveNotations "r1b1k1nr/pppppppp/8/8/8/8/PPPPPPPP/R1B1K1NR w KQkq - 1 2"
-        Assert.StrictEqual<string list>(castlingMoves, [])
+        Assert.StrictEqual<string list>([], castlingMoves)
     
 module Black = 
 
     [<Fact>]
     let ``Black castling both sides possible`` () =
         let castlingMoves = getPossibleCastlingMoveNotations "r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R b KQkq - 1 2"
-        Assert.StrictEqual<string list>(castlingMoves, ["0-0-0"; "0-0"])
+        Assert.StrictEqual<string list>(["0-0-0"; "0-0"], castlingMoves)
 
     [<Fact>]
     let ``Black castling not allowed from gameState`` () =
         let castlingMoves = getPossibleCastlingMoveNotations "r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R b KQ - 1 2"
-        Assert.StrictEqual<string list>(castlingMoves, [])
+        Assert.StrictEqual<string list>([], castlingMoves)
         
     [<Fact>]
     let ``Black castling blocked by piece`` () =
         let castlingMoves = getPossibleCastlingMoveNotations "r1b1k1nr/pppppppp/8/8/8/8/PPPPPPPP/R1B1K1NR b KQkq - 1 2"
-        Assert.StrictEqual<string list>(castlingMoves, [])
+        Assert.StrictEqual<string list>([], castlingMoves)
