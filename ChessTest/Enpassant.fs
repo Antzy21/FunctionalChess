8c99ff3fb7920a99e89f0b9db08dd6ad5e54110b
Anthony Dunford <trashzy21@gmail.com>
2022-06-14 20:09:36 +0100

    Add Enpassant tests

---
+++ ChessTest/Enpassant.fs
@@ -0,0 +1,26 @@
+﻿namespace Enpassant
+
+open Xunit
+open Chess
+open TestHelpers
+
+module White =
+
+    [<Fact>]
+    let ``White can take en passing two ways`` () =
+        let enpassantSquare = "c6"
+        let moves = 
+            $"rnbqkbnr/p7/Pp1p1p1p/1PpPpPpP/2P1P1P1/8/8/RNBQKBNR w KQkq {enpassantSquare} 0 1"
+            |> getPossibleMovesForPieceType Pawn
+        Assert.Contains("Pb5 -> xpc6", moves)
+        Assert.Contains("Pd5 -> xpc6", moves)
+    
+module Black = 
+    
+    [<Fact>]
+    let ``Black can take en passing`` () =
+        let enpassantSquare = "b3"
+        let moves = 
+            $"rnbqkbnr/2p5/2Pp1p1p/1p1PpPpP/pP2P1P1/P7/8/RNBQKBNR b KQkq {enpassantSquare} 0 1"
+            |> getPossibleMovesForPieceType Pawn
+        Assert.Contains("Pa4 -> xPb3", moves)
