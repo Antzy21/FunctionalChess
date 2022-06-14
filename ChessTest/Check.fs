37ba3da13a43f55abb3b8e61de145928369e93d8
Anthony Dunford <trashzy21@gmail.com>
2022-06-14 19:34:51 +0100

    Tests for pieces that can't move due as they block a check

--- ChessTest/Check.fs
+++ ChessTest/Check.fs
@@ -1,6 +1,7 @@
 ﻿namespace Check
 
 open Xunit
+open Chess
 open TestHelpers
 
 module White =
@@ -10,9 +11,21 @@ module White =
         gameStateIsInCheck "3kr3/8/8/8/8/8/8/4K3 w KQkq - 1 2"
         |> Assert.True
     
+    [<Fact>]
+    let ``White can't move pawn`` () =
+        "4k3/8/8/b7/8/2P5/8/4K3 w KQkq - 1 2"
+        |> getPossibleMovesForPieceType Pawn
+        |> Assert.Empty
+    
 module Black = 
 
     [<Fact>]
     let ``Black in check from Rook`` () =
         gameStateIsInCheck "4k3/8/8/8/8/8/8/4RK2 b KQkq - 1 2"
         |> Assert.True
+    
+    [<Fact>]
+    let ``Black can't move knight`` () =
+        "4k3/4n3/8/8/8/8/8/4RK2 b KQkq - 1 2"
+        |> getPossibleMovesForPieceType Knight
+        |> Assert.Empty
--- ChessTest/TestHelpers.fs
+++ ChessTest/TestHelpers.fs
@@ -1,6 +1,14 @@
 ﻿module internal TestHelpers
 
 open Chess
+open Checkerboard
+
+let getPossibleMovesForPieceType (pieceType: pieceType) (fen: string) : string list =
+    fen
+    |> GameState.fromFen
+    |> GameState.getMovesForPlayer
+    |> List.filter (Move.getMovedPiece >> fun p -> p.pieceType = pieceType)
+    |> List.map Move.getMoveNotation
 
 let getPossibleCastlingMoveNotations (fen: string) : string list =
     fen
