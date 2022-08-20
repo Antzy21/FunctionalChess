c2421ec4e1e41603cc5dd44e1e80e224192b6102
Anthony Dunford <30522408+Antzy21@users.noreply.github.com>
2022-08-20 14:28:09 +0100

    Promotion tests

---
+++ ChessTest/Promotion.fs
@@ -0,0 +1,15 @@
+﻿namespace Promotion
+
+open Xunit
+open Chess
+open TestHelpers
+
+module White =
+
+    [<Fact>]
+    let ``White can take promote`` () =
+        let moves = 
+            $"8/P7/8/8/8/8/8/2k2K3 w - - 0 1"
+            |> getPossibleMovesForPieceType Pawn
+        let expected = ["Pa7 -> Qa8"; "Pa7 -> Ra8"; "Pa7 -> Ba8"; "Pa7 -> Na8"]
+        Assert.Equal<string list>(expected, moves)
