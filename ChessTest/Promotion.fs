9e58b7e76efa18f37785a750d4e0260906ba20bc
Anthony Dunford <30522408+Antzy21@users.noreply.github.com>
2022-08-26 18:40:46 +0100

    Add promotion tests checking bord state

--- ChessTest/Promotion.fs
+++ ChessTest/Promotion.fs
@@ -7,9 +7,18 @@ open TestHelpers
 module White =
 
     [<Fact>]
-    let ``White can take promote`` () =
+    let ``White can promote`` () =
+        let result = 
+            "8/P7/8/8/8/8/8/2k2K2 w - - 0 1"
+            |> GameState.fromFen
+            |> GameState.makeMoveFromNotation "a8 = Q"
+            |> GameState.toFen
+        Assert.Equal("Q7/8/8/8/8/8/8/2k2K2 b - - 1 2", result)
+
+    [<Fact>]
+    let ``White can promote move list`` () =
         let moves = 
-            $"8/P7/8/8/8/8/8/2k2K3 w - - 0 1"
+            "8/P7/8/8/8/8/8/2k2K2 w - - 0 1"
             |> getPossibleMovesForPieceType Pawn
         let expected = ["Pa7 -> a8 = Q"; "Pa7 -> a8 = R"; "Pa7 -> a8 = B"; "Pa7 -> a8 = N"]
         Assert.Equal<string list>(expected, moves)
@@ -17,14 +26,23 @@ module White =
     [<Fact>]
     let ``Blocked from promoting`` () =
         let moves = 
-            $"b7/P7/8/8/8/8/8/2k2K3 w - - 0 1"
+            "b7/P7/8/8/8/8/8/2k2K2 w - - 0 1"
             |> getPossibleMovesForPieceType Pawn
         Assert.Empty(moves)
 
     [<Fact>]
     let ``Promoting by taking`` () =
+        let result = 
+            "br6/P7/8/8/8/8/8/2k2K2 w - - 0 1"
+            |> GameState.fromFen
+            |> GameState.makeMoveFromNotation "xb8 = Q"
+            |> GameState.toFen
+        Assert.Equal("bQ6/8/8/8/8/8/8/2k2K2 b - - 1 2", result)
+
+    [<Fact>]
+    let ``Promoting by taking move list`` () =
         let moves = 
-            $"br6/P7/8/8/8/8/8/2k2K3 w - - 0 1"
+            "br6/P7/8/8/8/8/8/2k2K2 w - - 0 1"
             |> getPossibleMovesForPieceType Pawn
         let expected = ["Pa7 -> xb8 = Q"; "Pa7 -> xb8 = R"; "Pa7 -> xb8 = B"; "Pa7 -> xb8 = N"]
         Assert.Equal<string list>(expected, moves)
--- NotationParser.fs
+++ NotationParser.fs
@@ -4,6 +4,57 @@ open Checkerboard
 open FSharp.Extensions
 
 module NotationParser =
+    let private normalMoveParsing (colour: colour) (board: board) (move: string) : move option =
+        
+        let piece = 
+            if move.Length = 2 || move.Chars 0 = 'x' then
+                Some Pawn
+            else 
+                PieceType.tryParse (move.Chars 0)
+            |> Option.map (fun pieceType ->
+                {pieceType = pieceType; colour = colour}
+            )
+
+        let newSquare = 
+            move.[move.Length-2 ..]
+            |> Coordinates.tryParse
+            |> Option.map (fun newCoordinates ->
+                Board.GetSquare.fromCoordinates newCoordinates board
+            )
+
+        (piece, newSquare)
+        ||> Option.map2 (fun p ns -> (p, ns))
+        |> Option.bind (fun (piece, newSquare) ->
+            Board.GetSquares.reverseEngineerPieceLocations piece newSquare.coordinates board
+            |> function
+            | oldSquare :: [] ->
+                Some (oldSquare, newSquare)
+            | [] ->
+                printfn $"No {piece.pieceType} avaiable to do {move}"
+                None
+            | squares ->
+                printfn $"Too many {piece.pieceType}s are able to do {move}"
+                squares
+                |> List.iter (fun square -> printfn $"{Square.getDescription square}")
+                None
+        )
+        
+    let tryParseFullNotation (board: board) (move: string) : move option =
+        match move.Split(' ') with
+        | [|fstSquare; _; sndSquare |] ->
+            fstSquare.[fstSquare.Length-2 ..]
+            |> Coordinates.tryParse
+            |> Option.bind (fun fstCoordinates ->
+                let fstSquare = Board.GetSquare.fromCoordinates fstCoordinates board
+                sndSquare.[sndSquare.Length-2 ..]
+                |> Coordinates.tryParse
+                |> Option.bind (fun sndCoordinates ->
+                    let sndSquare = Board.GetSquare.fromCoordinates sndCoordinates board
+                    Some (fstSquare, sndSquare)
+                )
+            )
+        | _ -> None
+
     let tryParse (colour: colour) (board: board) (move: string) : move option =
         match move with
         | "0-0-0" ->
@@ -30,32 +81,18 @@ module NotationParser =
                     {piece = Some {pieceType = King; colour = colour}; coordinates = (3,7)},
                     {piece = None; coordinates = (1,7)}
                 )
-        | move ->
-            let newCoordinates = 
-                move.[move.Length-2 .. move.Length]
-                |> Coordinates.fromName
-            let pieceType =
-                if move.Length = 2 || move.Chars 0 = 'x' then
-                    Pawn
-                else 
-                    PieceType.fromLetter (move.Chars 0)
-            let piece = {pieceType = pieceType; colour = colour}
-
-            let newSquare = Board.GetSquare.fromCoordinates newCoordinates board
-
-            Board.GetSquares.reverseEngineerPieceLocations piece newSquare.coordinates board
-            |> function
-            | oldSquare :: [] ->
-                Some (oldSquare, newSquare)
-            | [] ->
-                printfn $"No {pieceType} avaiable to do {move}"
-                None
-            | squares ->
-                printfn $"Too many {pieceType}s are able to do {move}"
-                squares
-                |> List.iter (fun square -> printfn $"{Square.getDescription square}")
-                None
-
+        | move when move.Contains('=') ->
+            normalMoveParsing colour board (move.Split(' ').[0])
+            |> Option.map (fun parsedMove ->
+                let sndSquare = snd parsedMove
+                let sndSquare = Square.updateWithPiece (Piece.getFromLetter (move.[move.Length-1])) sndSquare
+                fst parsedMove, sndSquare
+            )
+        | move -> 
+            try
+                normalMoveParsing colour board move
+            with
+            _ -> tryParseFullNotation board move
     let parse (colour: colour) (board: board) (move: string) : move =
         tryParse colour board move
         |> Option.failOnNone "Failed to parse notation"
