e0b28cf76df20c4825c7e6603f68bace5909cca5
Anthony Dunford <trashzy21@gmail.com>
2024-01-06 21:28:49 +0000

    Add maps for pieces indifferent to colour

--- ChessModule/Board.Type.fs
+++ ChessModule/Board.Type.fs
@@ -24,27 +24,39 @@ type board =
         (this.pieceMap) &&& (this.ColourBitmap)
     member this.blackPieces =
         (this.pieceMap) &&& (~~~this.ColourBitmap)
+    member this.queenMap =
+        this.KQBRmap &&& this.KQPmap &&& (~~~ this.KPNRmap) 
     member this.whiteQueenMap =
-        this.ColourBitmap &&& this.KQBRmap &&& this.KQPmap &&& (~~~ this.KPNRmap) 
+        this.ColourBitmap &&& this.queenMap
     member this.blackQueenMap =
-        (~~~ this.ColourBitmap) &&& this.KQBRmap &&& this.KQPmap &&& (~~~ this.KPNRmap)
+        (~~~ this.ColourBitmap) &&& this.queenMap
+    member this.kingMap =
+        this.KQBRmap &&& this.KQPmap &&& this.KPNRmap
     member this.whiteKingMap =
-        this.ColourBitmap &&& this.KQBRmap &&& this.KQPmap &&& this.KPNRmap
+        this.ColourBitmap &&& this.kingMap
     member this.blackKingMap =
-        (~~~ this.ColourBitmap) &&& this.KQBRmap &&& this.KQPmap &&& this.KPNRmap
+        (~~~ this.ColourBitmap) &&& this.kingMap
+    member this.pawnMap =
+        (~~~ this.KQBRmap) &&& this.KQPmap &&& this.KPNRmap
     member this.whitePawnMap =
-        this.ColourBitmap &&& (~~~ this.KQBRmap) &&& this.KQPmap &&& this.KPNRmap
+        this.ColourBitmap &&& this.pawnMap
     member this.blackPawnMap =
-        (~~~ this.ColourBitmap) &&& (~~~ this.KQBRmap) &&& this.KQPmap &&& this.KPNRmap
+        (~~~ this.ColourBitmap) &&& this.pawnMap
+    member this.rookMap =
+        this.KQBRmap &&& (~~~ this.KQPmap) &&& this.KPNRmap
     member this.whiteRookMap =
-        this.ColourBitmap &&& this.KQBRmap &&& (~~~ this.KQPmap) &&& this.KPNRmap
+        this.ColourBitmap &&& this.rookMap
     member this.blackRookMap =
-        (~~~ this.ColourBitmap) &&& this.KQBRmap &&& (~~~ this.KQPmap) &&& this.KPNRmap
+        (~~~ this.ColourBitmap) &&& this.rookMap
+    member this.knightMap =
+        (~~~ this.KQBRmap) &&& (~~~ this.KQPmap) &&& this.KPNRmap
     member this.whiteKnightMap =
-        this.ColourBitmap &&& (~~~ this.KQBRmap) &&& (~~~ this.KQPmap) &&& this.KPNRmap
+        this.ColourBitmap &&& this.knightMap
     member this.blackKnightMap =
-        (~~~ this.ColourBitmap) &&& (~~~ this.KQBRmap) &&& (~~~ this.KQPmap) &&& this.KPNRmap
+        (~~~ this.ColourBitmap) &&& this.knightMap
+    member this.bishopMap =
+        this.KQBRmap &&& (~~~ this.KQPmap) &&& (~~~ this.KPNRmap)
     member this.whiteBishopMap =
-        this.ColourBitmap &&& this.KQBRmap &&& (~~~ this.KQPmap) &&& (~~~ this.KPNRmap)
+        this.ColourBitmap &&& this.bishopMap
     member this.blackBishopMap =
-        (~~~ this.ColourBitmap) &&& this.KQBRmap &&& (~~~ this.KQPmap) &&& (~~~ this.KPNRmap)
+        (~~~ this.ColourBitmap) &&& this.bishopMap
